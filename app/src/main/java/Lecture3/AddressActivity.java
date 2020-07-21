bc38c92254d1468d23695ee2249795cf305d3852
ihyobin <hb0617@hanyang.ac.kr>
2020-07-21 21:56:57 +0900

    주석

--- app/src/main/java/Lecture3/AddressActivity.java
+++ app/src/main/java/Lecture3/AddressActivity.java
@@ -88,7 +88,7 @@ public class AddressActivity extends AppCompatActivity {
         editTextFilter.addTextChangedListener(new TextWatcher(){
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // 입력란에 변화가 있 시 조치
+                // 입력란에 변화가 있을 시 조치
                 String filterText = s.toString();
                 search(filterText);
                 adapter.notifyDataSetChanged();
